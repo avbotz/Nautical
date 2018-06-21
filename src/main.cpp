06910f7792b1bbe3dcab6bc031d17eb87054f35c
David Zhang <davarco123@gmail.com>
2018-06-21 16:43:51 -0700

    Removed more stuff - at this point I'm just trigger happy.

--- src/main.cpp
+++ src/main.cpp
@@ -18,103 +18,30 @@
  */
 void run()
 {
-	// Serial << "Beginning Cuckical!" << '\n';
 	init_io();
 
-	// Set initial kill status.
-	bool killed = !alive();
-
-	// Initial power is 0, Aquastorm should set initial power.
 	float p = 0.0f;
-
-	// Create PID controllers for each degree of freedom our sub has.
 	PID controllers[DOF];
 	for (int i = 0; i < DOF; i++)
 		controllers[i].init(GAINS[i][0], GAINS[i][1], GAINS[i][2]);
 
-	// Current holds location and desired holds destination.
-	State current, desired;
-	compute_initial_state(current);
-
-	// Setup calculations for time difference. 
 	uint32_t start = micros();
 	uint32_t ptime = start;
 	uint32_t stime = start;
+
+	float *current = new float[DOF];
+	float *desired = new float[DOF];
 
 	while (true)
 	{
 		delay(10);
 		if (Serial.available() > 0)
 		{
-			/*
-			 * 's' = set state
-			 * 'p' = set power
-			 * 'c' = req state
-			 * 'a' = req kills
-			 * 'x' = terminate
-			 * 'd' = debug 
-			 * 'r' = reset
-			 */
 			char c = Serial.read();
-			switch (c)
-			{
-				case 's':
-					{
-						desired.read();
-						break;
-					}
-				case 'p':
-					{
-						p = Serial.parseFloat();
-						break;
-					}
-				case 'c':
-					{
-						current.print();
-						break;
-					}
-				case 'd':
-					{
-						desired.print();
-						break;
-					}
-				case 'a':
-					{
-						Serial << (alive() ? 1:0) << '\n';
-						break;
-					}
-				case 'x':
-					{
-						p = 0;
-						float temp[NUM_MOTORS] = { 0.0f };
-						set_motors(temp);
-						break;
-					}
-				case 'm':
-					{
-						int id = Serial.parseInt();
-						float k = Serial.parseFloat();
-						if (id == 0)
-						{
-							Serial << "Carl Hayden infiltrator detected! Shutting down." << '\n';
-							return;
-						}
-						set_motor(id, k);
-						break;
-					}
-				case 'r':
-					{
-						reset_state(current);
-						reset_state(desired);
-						break;
-					}
+			if ()
 			}
 		}
 
-
-		// Compute state difference.
-		// Managing multiple axes at one time increases drift in the sub, so it
-		// will be avoided until the DVL is integrated.
 		float dstate[DOF] = { 0.0f };
 		if (fabs(desired.z - current.z) > 0.05)
 			dstate[2] = desired.z - current.z;	
@@ -125,7 +52,6 @@ void run()
 			dstate[3] = calc_angle_diff(desired.yaw, current.yaw);
 		}
 
-		// Compute PID using state difference.
 		float dt = (float)(micros() - ptime)/(float)(1000000);	
 		float pid[DOF] = { 0.0f };
 		for (int i = 0; i < MOVE_DOF; i++)
@@ -133,10 +59,8 @@ void run()
 		pid[3] = controllers[3].calculate(dstate[3], dt, 0.025);
 		ptime = micros();
 
-		// Compute new state using AHRS data. 
 		stime = compute_state(current, pid, p, stime);
 
-		// Move sub towards the desired location. 
 		compute_motors(dstate, pid, p);	
 	}
 }

7f185aa3ab1ed355829749fb4bb80e12197d889a
bbworld <vw1212@pleasantonusd.net>
2019-07-12 00:07:13 -0700

    Added logging option for remote control and made it Python3 compatible.

--- include/config.h
+++ include/config.h
@@ -4,7 +4,7 @@
  *  @author David Zhang
  */
 #ifndef CONFIG_H
-#define CONFIG_H 
+#define CONFIG_H
 
 /*! @name Runtime configuration.
  */
@@ -13,7 +13,7 @@
  *
  *  You can set this to true even if Nautical is being compiled on land, but
  *  make sure the kill switch isn't set to alive until the sub is in the water.
- *  Otherwise, the entire program will hang and stop producing output. 
+ *  Otherwise, the entire program will hang and stop producing output.
  */
 static const bool DVL_ON = false;
 
@@ -29,7 +29,7 @@ static const bool USE_INITIAL_HEADING = true;
  *
  *  The idea was to have the sub use an absolute value for north rather than one
  *  relative to unkill. Since this would depend less on the diver, it would be
- *  more consistent. 
+ *  more consistent.
  */
 static const bool FAR = true;
 
@@ -40,7 +40,7 @@ static const bool FAR = true;
 static const bool SIM = false;
 ///@}
 
-/*! @name Constants for degrees of freedom with North-East-Down coordinates. 
+/*! @name Constants for degrees of freedom with North-East-Down coordinates.
  */
 ///@{
 /** The number of degrees of freedom.
@@ -83,7 +83,7 @@ static const int R = 5;
 /*! @name Motor ID configuration.
  */
 ///@{
-/** Thruster enum makes code more readable. Do NOT set an ID to 0, the motor 
+/** Thruster enum makes code more readable. Do NOT set an ID to 0, the motor
  *  will brick.
  */
 enum thruster
@@ -106,7 +106,7 @@ static const int NUM_MOTORS = 8;
 
 /*! @name Motor orientation configuration.
  */
-/** Rows are motors while columns are directions. Positive numbers are for 
+/** Rows are motors while columns are directions. Positive numbers are for
  *  clockwise rotation and negative numbers are for counterclockwise.
  *
  *  The multipliers are either -1, 1, or 0 because they are meant to be
@@ -115,7 +115,7 @@ static const int NUM_MOTORS = 8;
  *  supposed to correct for that, then it would be a good idea to change the
  *  values back to 1.
  */
-static const float ORIENTATION[8][6] = 
+static const float ORIENTATION[8][6] =
 {
 	{ 0.00, 0.00, 1.00, 0.00, 1.00, -1.0 },
 	{ 0.00, 0.00, -1.0, 0.00, -1.0, -1.0 },
@@ -123,7 +123,7 @@ static const float ORIENTATION[8][6] =
 	{ 0.00, 0.00, 1.00, 0.00, -1.0, 1.00 },
 	{ 1.00, 1.00, 0.00, 1.00, 0.00, 0.00 },
 	{ -1.1, 1.00, 0.00, 1.00, 0.00, 0.00 },
-	{ -1.1, 1.00, 0.00, -1.0, 0.00, 0.00 }, 
+	{ -1.1, 1.00, 0.00, -1.0, 0.00, 0.00 },
 	{ 1.00, 1.00, 0.00, -1.0, 0.00, 0.00 }
 };
 
@@ -131,9 +131,9 @@ static const float ORIENTATION[8][6] =
  */
 /** Rows correspond to F, H, V, Y, P, and R while columns correspond to kp, ki,
  *  and kd. To be honest, these gains are not great. The integral component is
- *  being ignored at the moment because it doesn't work as well in practice. 
+ *  being ignored at the moment because it doesn't work as well in practice.
  */
-static const float GAINS[6][3] = 
+static const float GAINS[6][3] =
 {
 	{ 1.50, 0.00, 0.00 },
 	{ 1.50, 0.00, 0.00 },
@@ -150,4 +150,4 @@ static const float GAINS[6][3] =
 #define R2D 180./3.1415
 ///@}
 
-#endif 
+#endif
--- manual.py
+++ manual.py
@@ -1,15 +1,16 @@
-import keyboard, time, socket 
+import keyboard, time, socket
 
-print "Attempting to connect. Ensure that the sub-side node is running."
+print ("Attempting to connect. Ensure that the sub-side node is running.")
 s = socket.socket()
 port = 8080
 s.connect(('192.168.2.50', port))
-print "Finished connecting."
+print ("Finished connecting.")
+logging = True
 
 while True:
     time.sleep(0.5)
-    state = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    if keyboard.is_pressed('w'): 
+    state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    if keyboard.is_pressed('w'):
         state[0] = 1
     if keyboard.is_pressed('a'):
         state[1] = 1
@@ -27,13 +28,33 @@ while True:
         state[7] = 1
     if keyboard.is_pressed('q'):
         state[8] = 1
+    if keyboard.is_pressed('l'):
+        # Toggle logging of current state
+        print("Logging: " + logging)
+        logging = not logging
+
+    if logging:
+        # Note: We are piggybacking log state on the state data here.
+        # This is OK because of the way the `w` command is implemented;
+        # it will only read the first NUM_MOTORS numbers (0-7 here).
+        # This means we should be able to piggyback data without issues.
+        state[9] = 1
+
+        # Fetch state
+        fetchbuf = s.recv(1024).decode("utf-8")
+
+        # Match first state that comes up (most recent one)
+        current_state = fetchbuf[fetchbuf.find("("):fetchbuf.find(")")]
+        print(current_state)
+
     out = 'w '
     for i in state:
         out += str(i) + ' '
-    out += '\n';
+    out += '\n'
+
     s.send(out)
-    print '\n' + str(state)
+    print(out)
+    print ('\n' + str(state))
     if state[8] == 1:
         break
-print "Finished using manual control."
-
+print ("Finished using manual control.")
